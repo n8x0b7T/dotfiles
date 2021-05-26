8ad89a99e357cd11a918063cc11458d63d0afa91
BuildTools <deretilium@protonmail.com>
2021-05-26 13:43:59 -0400

    beautiful

--- install.sh
+++ install.sh
@@ -1,1 +1,19 @@
-find . -type f | grep -Ev ".git|intsall.sh"
+#!/bin/bash
+
+#install all the things
+# apt install git nmap htop vim tmux xrpd aircrack-ng python-pip openvpn ffmpeg gobuster terminator fish mpv wireshark bat
+# pip3 install updog;
+
+create_links (){
+    dotdir=~/.dotfiles
+    x=$(echo $1 | cut -c 2-)
+    echo "$x"
+    mkdir -p $(echo ~/test$x | rev | cut -d'/' -f2- | rev)
+    ln -sfn ~/.dotfiles$x ~/test$x
+}
+
+export -f create_links
+
+find . -type f -not -path "./.git/*" -not -path "*.sh" -exec bash -c 'create_links "{}"' \;
+
+
