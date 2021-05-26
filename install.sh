38a7aba5eb5e26068f3d5d8ebcfd604dc7937027
BuildTools <deretilium@protonmail.com>
2021-05-26 14:01:15 -0400

    um

--- install.sh
+++ install.sh
@@ -4,12 +4,19 @@
 # apt install git nmap htop vim tmux xrpd aircrack-ng python-pip openvpn ffmpeg gobuster terminator fish mpv wireshark bat
 # pip3 install updog;
 
+#find all config files, make necesary folders, create sym links
 create_links (){
+    #location of dotfiles
     dotdir=~/.dotfiles
+    #location of home dir
+    homedir=~
+
     x=$(echo $1 | cut -c 2-)
-    echo "$x"
-    mkdir -p $(echo ~/test$x | rev | cut -d'/' -f2- | rev)
-    ln -sfn ~/.dotfiles$x ~/test$x
+    # echo "$x"
+    # echo $homedir$x
+    echo $homedir$x | rev | cut -d'/' -f2- | rev
+    mkdir -p $(echo $homedir$x | rev | cut -d'/' -f2- | rev)
+    ln -sfn $dotdir$x $homedir$x
 }
 
 export -f create_links
