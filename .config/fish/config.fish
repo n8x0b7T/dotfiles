ea0e110bb3f0590b9240f493f9280b02c7ba1625
n8x0b7T <deretilium@protonmail.com>
2021-08-08 19:55:56 -0400

    add ip alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,3 +2,4 @@ set fish_greeting
 alias bat "batcat"
 alias open "xdg-open"
 alias myip "curl https://ifconfig.io"
+alias getips "ip a s | grep -v vmnet | grep -v docker | egrep -o 'inet [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d' ' -f2"
