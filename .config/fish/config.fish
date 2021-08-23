671a5f65963816635f82f91f024e3f47c424727b
n8x0b7T <deretilium@protonmail.com>
2021-08-23 17:28:24 -0400

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,4 +2,6 @@ set fish_greeting
 alias bat "batcat"
 alias open "xdg-open"
 alias myip "curl https://ifconfig.io"
-alias getips "ip a s | grep -v vmnet | grep -v docker | egrep -o 'inet [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d' ' -f2"
+alias getips "ip a s | grep -v vmnet | grep -v docker | grep -v virbr | egrep -o 'inet [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d' ' -f2"
+alias changemac "sudo ip link set wlx98482790fc7d down; sudo macchanger -A wlx98482790fc7d; sudo ip link set wlx98482790fc7d up"
+
