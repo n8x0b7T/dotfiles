f817802b416f5729ea4c5fb9cb3f9232b481dab4
n8x0b7T <deretilium@protonmail.com>
2022-02-15 21:20:07 -0500

    pwncat

--- .bashrc
+++ .bashrc
@@ -123,3 +123,5 @@ fi
 
 
 . "$HOME/.cargo/env"
+
+
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,7 @@ alias myip "curl https://ifconfig.io"
 alias getips "ip a s | grep -v vmnet | grep -v docker | grep -v virbr | egrep -o 'inet [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d' ' -f2"
 alias changemac "sudo ip link set wlx98482790fc7d down; sudo macchanger -A wlx98482790fc7d; sudo ip link set wlx98482790fc7d up"
 alias copyt "xclip -sel c"
+alias pwncat pwncat-cs
 begin
     set --local AUTOJUMP_PATH /usr/share/autojump/autojump.fish
     if test -e $AUTOJUMP_PATH
