0ded7b5967db09c7a0c3287e41c4ef9193d0fc1c
n8x0b7T <deretilium@protonmail.com>
2023-10-04 09:44:56 -0400

    clear higlights, silence move line

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -5,9 +5,11 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 vim.keymap.set("i", "<C-c>", "<Esc>")
+-- clear search highlights
+vim.keymap.set("n", "<C-c>", "<cmd>noh<cr>")
 
 -- move lines in visual mode
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { silent = true })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { silent = true })
 
-vim.keymap.set('n', '<C-L>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>')
+vim.keymap.set("n", "<C-L>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>")
