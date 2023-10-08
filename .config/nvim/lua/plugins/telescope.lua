feaa45bf55ecc24f6a588b96d69c3b2b0b4b10e0
n8x0b7T <deretilium@protonmail.com>
2023-10-08 16:20:45 -0400

    hot fix

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -33,7 +33,7 @@ return {
             },
             file_ignore_patterns = {
                 "node_modules/",
-                "^.git/",
+                ".git/",
                 "%.lock",
             }
         },
