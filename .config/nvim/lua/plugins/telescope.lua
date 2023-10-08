84c103b1aaa10d25efcbce5f0811ed77f63d6dda
n8x0b7T <deretilium@protonmail.com>
2023-10-07 22:25:23 -0400

    ignore .git

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -11,7 +11,7 @@ return {
         vim.keymap.set('n', '<leader>fs', builtin.grep_string, {})
         vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-        vim.keymap.set('n', '<leader>fd', "<cmd>lua require('telescope.builtin').find_files({ hidden = true })<cr>",
+        vim.keymap.set('n', '<leader>fd', "<cmd>lua require('telescope.builtin').find_files({ hidden = true, file_ignore_patterns = { 'node_modules', '.git' }})<cr>",
             { silent = true })
     end
 }
