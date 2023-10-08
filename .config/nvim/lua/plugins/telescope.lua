4ff811d06251d056763bede847da0fb1df5c25a8
n8x0b7T <deretilium@protonmail.com>
2023-10-08 13:54:08 -0400

    much better telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,16 +2,40 @@ return {
     "nvim-telescope/telescope.nvim",
     branch = "0.1.x",
     event = "VeryLazy",
-    dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons" },
-    config = function()
-        -- set keymaps
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-        vim.keymap.set('n', '<leader>fs', builtin.grep_string, {})
-        vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-        vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-        vim.keymap.set('n', '<leader>fd', "<cmd>lua require('telescope.builtin').find_files({ hidden = true, file_ignore_patterns = { 'node_modules', '.git' }})<cr>",
-            { silent = true })
-    end
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons"
+    },
+    keys = {
+        { "<leader>ff", "<cmd>Telescope find_files<cr>" },
+        { "<leader>fg", "<cmd>Telescope live_grep<cr>" },
+        { "<leader>fs", "<cmd>Telescope grep_string<cr>" },
+        { "<leader>fb", "<cmd>Telescope buffers<cr>" },
+        { "<leader>fh", "<cmd>Telescope help_tags<cr>" },
+    },
+    opts = {
+        pickers = {
+            find_files = {
+                hidden = true
+            }
+        },
+        defaults = {
+            vimgrep_arguments =
+            {
+                "rg",
+                "--color=never",
+                "--no-heading",
+                "--with-filename",
+                "--line-number",
+                "--column",
+                "--smart-case",
+                "--hidden"
+            },
+            file_ignore_patterns = {
+                "node_modules/",
+                "^.git/",
+                "%.lock",
+            }
+        },
+    }
 }
