e56ad6a077b77f97bf2ea68df05dc5205d488fc4
n8x0b7T <deretilium@protonmail.com>
2023-10-09 13:27:37 -0400

    remove pyright

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -66,7 +66,7 @@ return {
             end)
 
             require('mason-lspconfig').setup({
-                ensure_installed = { "pylsp", "pyright", "clangd", "lua_ls", "cssls", "bashls", "hls" ,"jdtls"},
+                ensure_installed = { "pylsp", "clangd", "lua_ls", "cssls", "bashls", "hls" ,"jdtls"},
                 handlers = {
                     lsp_zero.default_setup,
                     lua_ls = function()
