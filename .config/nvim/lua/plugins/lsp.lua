88e4fb31edfb0c5b60f348c248a23590e7e7f1b4
n8x0b7T <you@example.com>
2025-02-25 09:27:30 -0500

    ruff

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -66,7 +66,7 @@ return {
             end)
 
             require('mason-lspconfig').setup({
-                ensure_installed = { "pylsp", "clangd", "lua_ls", "cssls", "bashls", "jdtls"},
+                ensure_installed = { "ruff", "pyright", "clangd", "lua_ls", "cssls", "bashls", "jdtls" },
                 handlers = {
                     lsp_zero.default_setup,
                     lua_ls = function()
@@ -76,6 +76,42 @@ return {
                     end,
                 }
             })
+
+            require('lspconfig').ruff.setup({
+                init_options = {
+                    settings = {
+                        -- Server settings should go here
+                    }
+                }
+            })
+            vim.api.nvim_create_autocmd("LspAttach", {
+                group = vim.api.nvim_create_augroup('lsp_attach_disable_ruff_hover', { clear = true }),
+                callback = function(args)
+                    local client = vim.lsp.get_client_by_id(args.data.client_id)
+                    if client == nil then
+                        return
+                    end
+                    if client.name == 'ruff' then
+                        -- Disable hover in favor of Pyright
+                        client.server_capabilities.hoverProvider = false
+                    end
+                end,
+                desc = 'LSP: Disable hover capability from Ruff',
+            })
+            require('lspconfig').pyright.setup {
+                settings = {
+                    pyright = {
+                        -- Using Ruff's import organizer
+                        disableOrganizeImports = true,
+                    },
+                    python = {
+                        analysis = {
+                            -- Ignore all files for analysis to exclusively use Ruff for linting
+                            ignore = { '*' },
+                        },
+                    },
+                },
+            }
         end
     }
 }
